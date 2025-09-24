bf7299c1d335b9dadcfc70b9dad29bb241b45382
sunday <iamdyclef@gmail.com>
2025-09-24 18:17:35 +0100

    feat: done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Sponsors from './components/Sponsors'
-import Speech from './Components/Speech'
-import PoweringFuture from './Components/PoweringFuture'
-import SaterliteLauch from './Components/SaterliteLauch'
-import MoonWatch from './Components/MoonWatch'
-import GlobalImpact from './Components/GlobalImpact'
-import Team from './Components/Team'
+import Speech from './components/Speech'
+import PoweringFuture from './components/PoweringFuture'
+import SaterliteLauch from './components/SaterliteLauch'
+import MoonWatch from './components/MoonWatch'
+import GlobalImpact from './components/GlobalImpact'
+import Team from './components/Team'
 import Newsletter from './components/Newsletter'
-import Footer from './Components/Footer'
+import Footer from './components/Footer'
 
 
 const App = () => {
